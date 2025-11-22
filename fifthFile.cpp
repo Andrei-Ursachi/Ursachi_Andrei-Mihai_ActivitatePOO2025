d85b4460588be6adcfacd904a04de1f426c50c88
Ursa1201 <andrei.ursachi97@gmail.com>
2025-11-22 21:53:02 +0200

    changed some stuff in fifthFile to fix some issues, added new file for exercises

--- fifthFile.cpp
+++ fifthFile.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+﻿#include<iostream>
 #include<fstream>
 
 using namespace std;
 
 class Petitie {
-public:
+private:
 	const int id;
 	string data;
 	string numeComplet;
@@ -12,7 +12,9 @@ public:
 	bool status;
 	char* descriere;
 
-	//no param
+public:
+
+	//constructor 1 - fara parametri
 	Petitie() :id(0) {
 		this->data = "azi";
 		this->numeComplet = "Constructor unu";
@@ -21,12 +23,14 @@ public:
 		this->descriere = nullptr;
 	}
 
-	Petitie(string data, string numeComplet, int nrZile, bool status, char* descriere) :id(id) {
+	//constructor 2 - cu parametri
+	Petitie(string data, string numeComplet, int nrZile, bool status, char* descriere) :id(1) {
 		this->data = data;
 		this->numeComplet = numeComplet;
 		this->nrZile = nrZile;
 		this->status = status;
-		if (this->descriere != nullptr) {
+
+		if (descriere != nullptr) {
 			this->descriere = new char[strlen(descriere) + 1];
 			strcpy_s(this->descriere, strlen(descriere) + 1, descriere);
 		}
@@ -35,12 +39,14 @@ public:
 		}
 	}
 
+	//constructor copiere
 	Petitie(const Petitie& pet) :id(pet.id) {
 		this->data = pet.data;
 		this->numeComplet = pet.numeComplet;
 		this->nrZile = pet.nrZile;
 		this->status = pet.status;
-		if (this->descriere != nullptr) {
+
+		if (pet.descriere != nullptr) {
 			this->descriere = new char[strlen(pet.descriere) + 1];
 			strcpy_s(this->descriere, strlen(pet.descriere) + 1, pet.descriere);
 		}
@@ -49,48 +55,100 @@ public:
 		}
 	}
 
+	//destructor
 	~Petitie() {
 		if (this->descriere != nullptr) {
-			delete[]this->descriere;
+			delete[] this->descriere;
 		}
 		cout << "aici se apeleaza destructorul" << endl;
 	}
 
+	//operator <<
 	friend ostream& operator<<(ostream& out, const Petitie& p) {
 		out << p.id << endl;
 		out << p.data << endl;
 		out << p.numeComplet << endl;
 		out << p.nrZile << endl;
 		out << p.status << endl;
+
 		if (p.descriere != nullptr) {
 			out << p.descriere << endl;
 		}
 		else {
 			out << "Nu exista valoare pt. descriere!" << endl;
 		}
-		return out;
-	}	
 
+		return out;
+	}
+
+	//operator =
 	Petitie& operator=(const Petitie& pet) {
+		if (this == &pet) return *this;
+
 		if (this->descriere != nullptr) {
-			delete[]this->descriere;
+			delete[] this->descriere;
 		}
+
 		this->data = pet.data;
 		this->numeComplet = pet.numeComplet;
 		this->nrZile = pet.nrZile;
 		this->status = pet.status;
-		if (this->descriere != nullptr) {
+
+		if (pet.descriere != nullptr) {
 			this->descriere = new char[strlen(pet.descriere) + 1];
 			strcpy_s(this->descriere, strlen(pet.descriere) + 1, pet.descriere);
 		}
 		else {
 			this->descriere = nullptr;
 		}
+
 		return *this;
 	}
+
+	bool operator!() {
+		if (status == 0 && nrZile > 30) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void operator~() {
+		if (status == 0) {
+			status = 1;
+		}
+		else {
+			status = 0;
+		}
+	}
+
+	//getters
+	string getData() {
+		return this->data;
+	}
+
+	string getNumeComplet() {
+		return this->numeComplet;
+	}
+
+	int getNrZile() {
+		return this->nrZile;
+	}
+
+	bool getStatus() {
+		return this->status;
+	}
+
+	char* getDescriere() {
+		return this->descriere;
+	}
+
 };
 
+
 void main() {
+
 	Petitie p1;
 	cout << p1 << endl;
 
@@ -98,6 +156,23 @@ void main() {
 	Petitie p2("Anul 2002", "Reclamatie", 2, true, text);
 	cout << p2 << endl;
 
-	Petitie p3 = p2;
+	Petitie p3 = p2; //constructor copiere
 	cout << p3 << endl;
+
+	bool ok = !p1;
+	cout << (ok ? "Expirata" : "Ok") << endl;
+
+	p1.operator~();  //apel operator schimbare status
+	cout << "Aici e apelat ~" << endl << p1 << endl;
+
+	cout << p1.getData() << endl;
+	cout << p1.getNumeComplet() << endl;
+	cout << p1.getNrZile() << endl;
+	cout << p1.getStatus() << endl;
+
+	
+	if (p1.getDescriere() != nullptr)
+		cout << p1.getDescriere() << endl;
+	else
+		cout << "Descriere NULL" << endl;
 }
